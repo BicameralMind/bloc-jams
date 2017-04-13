d4ab68a71bd38b593896e00f53b378837c1d7d16
Lauren Soni <Buzz@Laurens-iMac.home>
2017-04-12 22:49:08 -0400

    checkpoint-9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,6 @@
-var animatePoints = function() {
-    
-    var points = document.getElementsByClassName('point');
-                
-      /*        var revealPoint = function() {
-                    for(var i =0; i<= points[i]; i++) {
-                        points[i].style.opacity = 1;
-                        points[i].style.transform = "scaleX(1) translateY(0)";
-                        points[i].style.msTransform = "scaleX(1) translateY(0)";
-                        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-                    }  
-                }
-               revealPoint();
-      */   
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
     
     var revealPoint = function(index) {
         points[index].style.opacity = 1;
@@ -23,32 +12,23 @@ var animatePoints = function() {
     for(var i =0; i< points.length; i++) {
         revealPoint(i);
     }
-    
-    
-    
-/*    var revealFirstPoint = function() {    
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
-*/     
 };
+
+
+window.onload = function() {
+    //automatically animate the points on a tall screen where scrolling can't trigger animation
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+     });
+}
+
+
