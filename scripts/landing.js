af5499f347d88be9b9991cd114cf5f9995838f39
Lauren Soni <Buzz@Laurens-iMac.home>
2017-05-04 13:57:35 -0400

    checkpoing16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,3 +1,5 @@
+/*
+
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
@@ -33,4 +35,59 @@ window.onload = function() {
      });
 }
 
+*/
+
+/*
+
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+     };
+    
+    $.each($('.point'), revealPoint);  
+};
+
+$(window).load(function() {
+   if ($(window).height() > 950) {
+         animatePoints();
+   }
+   
+   var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+   $(window).scroll(function(event) {
+       if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+       }
+    });
+    
+});
+
+*/
+
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    $.each($('.point'), revealPoint);
+};
+
+$(window).load(function() {
+     if ($(window).height() > 950) {
+         animatePoints();
+     }
+    
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+    $(window).scroll(function(event) {
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+         }
+    });
+});
 
